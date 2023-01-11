a67c0fe41327c7946ce7008b73e3004ef08cb99f
TheRealLorenz <lollobellina@gmail.com>
2023-01-11 13:47:50 +0100

    Add mini.bufremove

--- lua/user/plugins/init.lua
+++ lua/user/plugins/init.lua
@@ -44,4 +44,15 @@ return {
     lazy = false,
   },
 
+  {
+    'echasnovski/mini.bufremove',
+    keys = {
+      { "<leader>c", function() MiniBufremove.wipeout() end, desc = "Close Buffer" },
+    },
+    config = function() 
+        require('mini.bufremove').setup() 
+    end,
+    lazy = true,
+  },
+
 }
