3b25f00f4893987fbfdfcff8e8b3c792ffd8c7fc
TheRealLorenz <lollobellina@gmail.com>
2023-01-15 15:11:13 +0100

    Add colorscheme directive in lazy installer

--- init.lua
+++ init.lua
@@ -4,6 +4,9 @@ require("lazy").setup("user.plugins", {
   defaults = {
     lazy = true,
   },
+  install = {
+    colorscheme = { "tokyonight" },
+  },
 })
 require("user.mappings")
 require("user.autocmds")
