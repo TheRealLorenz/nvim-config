b219a330483ea0750b04f84d887a5a7da87a85ea
TheRealLorenz <lollobellina@gmail.com>
2023-01-21 12:54:53 +0100

    Move around splits in terminal mode

--- lua/user/mappings.lua
+++ lua/user/mappings.lua
@@ -15,6 +15,12 @@ vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", options)
 vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", options)
 vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", options)
 
+-- Move around splits using Ctrl + {h,j,k,l} in terminal mode
+vim.api.nvim_set_keymap("t", "<C-h>", "<C-\\><C-n><C-w>h", options)
+vim.api.nvim_set_keymap("t", "<C-j>", "<C-\\><C-n><C-w>j", options)
+vim.api.nvim_set_keymap("t", "<C-k>", "<C-\\><C-n><C-w>k", options)
+vim.api.nvim_set_keymap("t", "<C-l>", "<C-\\><C-n><C-w>l", options)
+
 -- Move around buffers
 vim.api.nvim_set_keymap("n", "H", "<cmd>bp<cr>", options)
 vim.api.nvim_set_keymap("n", "L", "<cmd>bn<cr>", options)
