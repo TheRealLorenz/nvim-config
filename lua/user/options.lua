e46bbcdb1c6448af338797615c3992bfac99bc2d
TheRealLorenz <lollobellina@gmail.com>
2023-01-22 11:36:22 +0100

    Refactor options

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,16 +1,16 @@
-local options = {
-  tabstop = 4,
-  shiftwidth = 4,
-  expandtab = true,
-  number = true,
-  relativenumber = true,
-  mouse = "a",
-}
+return {
+  opt = {
+    tabstop = 4,
+    shiftwidth = 4,
+    expandtab = true,
+    number = true,
+    relativenumber = true,
+    mouse = "a",
+  },
 
-local globals = {
-  mapleader = " ",
-  loaded_netrw = 1,
-  loaded_netrwPlugin = 1,
+  g = {
+    mapleader = " ",
+    loaded_netrw = 1,
+    loaded_netrwPlugin = 1,
+  },
 }
-
-return { g = globals, opt = options }
