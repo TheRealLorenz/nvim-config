0e2cbb8e9f1ae72571f25e4f10d833fb91486f62
TheRealLorenz <lollobellina@gmail.com>
2023-01-25 10:14:44 +0100

    Update sumneko_lua configs

--- lua/user/lsp/servers/sumneko_lua.lua
+++ lua/user/lsp/servers/sumneko_lua.lua
@@ -3,5 +3,11 @@ return {
     diagnostics = {
       globals = { "vim" },
     },
+    workspace = {
+      library = {
+        [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+        [vim.fn.stdpath("config") .. "/lua"] = true,
+      },
+    },
   },
 }
