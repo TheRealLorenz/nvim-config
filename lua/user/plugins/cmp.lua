02b0c6ac2b4d3c4690a62995c26480e9ba1e20d9
TheRealLorenz <lollobellina@gmail.com>
2023-02-04 17:44:31 +0100

    Add friendly-snippets

--- lua/user/plugins/cmp.lua
+++ lua/user/plugins/cmp.lua
@@ -11,6 +11,16 @@ end
 
 return {
   {
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+    },
+    config = function()
+      require("luasnip.loaders.from_vscode").lazy_load()
+    end,
+  },
+
+  {
     "hrsh7th/nvim-cmp",
     dependencies = {
       "hrsh7th/cmp-nvim-lsp",
