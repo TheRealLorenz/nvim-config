46bea62560f038349bf956bb25926ab0d1de5e0d
TheRealLorenz <lollobellina@gmail.com>
2023-02-08 15:55:35 +0100

    Add nvim-ts-autotag

--- lua/user/plugins/init.lua
+++ lua/user/plugins/init.lua
@@ -20,7 +20,15 @@ return {
     "nvim-treesitter/nvim-treesitter",
     dependencies = {
       "nvim-treesitter/nvim-treesitter-textobjects",
+      "windwp/nvim-ts-autotag",
     },
     event = "BufReadPre",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        autotag = {
+          enable = true,
+        },
+      })
+    end,
   },
 }
