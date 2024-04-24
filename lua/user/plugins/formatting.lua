0d9726654d34153bccdf8dcc6df17a0a837371d1
TheRealLorenz <lollobellina@gmail.com>
2024-04-24 11:49:04 +0200

    Move mapping to specific lazy field

--- lua/user/plugins/formatting.lua
+++ lua/user/plugins/formatting.lua
@@ -3,11 +3,6 @@ return {
   config = function()
     vim.g.auto_format = true
 
-    vim.keymap.set('n', '<leader>tf', function()
-      vim.g.auto_format = not vim.g.auto_format
-      vim.notify('Auto format: ' .. tostring(vim.g.auto_format))
-    end, { desc = 'Auto Format' })
-
     require('conform').setup {
       formatters_by_ft = require('user.mason-helper').formatters_by_ft(),
     }
@@ -21,4 +16,16 @@ return {
       end,
     })
   end,
+  event = 'BufReadPre',
+  keys = {
+    {
+      '<leader>tf',
+      function()
+        vim.g.auto_format = vim.g.auto_format or false
+        vim.g.auto_format = not vim.g.auto_format
+        vim.notify('Auto format: ' .. tostring(vim.g.auto_format))
+      end,
+      desc = 'Auto Format',
+    },
+  },
 }
