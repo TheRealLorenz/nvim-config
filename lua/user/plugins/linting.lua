f3384080a08e7d140d9ed522b0966f5360916cb6
TheRealLorenz <lollobellina@gmail.com>
2024-04-25 17:31:47 +0200

    Add LintInfo command

--- lua/user/plugins/linting.lua
+++ lua/user/plugins/linting.lua
@@ -1,17 +1,36 @@
+---Returns a string with the configured linters
+---for filetype.
+---@param ft string? Filetype, nil for current buffer
+---@return string
+local function lint_info(ft)
+  ft = ft or vim.bo.filetype
+  local linters = require('lint')._resolve_linter_by_ft(ft)
+
+  if #linters == 0 then
+    return 'No configured linters for \'' .. ft .. '\''
+  end
+
+  return 'Configured linters for \''
+    .. ft
+    .. '\': '
+    .. table.concat(linters, ', ')
+end
+
 return {
   'mfussenegger/nvim-lint',
   dependencies = {
     'williamboman/mason.nvim',
   },
   init = function()
-    vim.api.nvim_create_user_command('LintInfo', function()
-      vim.print(vim.inspect(require('lint').get_running()))
-    end, {})
+    vim.api.nvim_create_user_command('LintInfo', function(ctx)
+      vim.print(lint_info(ctx.fargs[1]))
+    end, { nargs = '?' })
   end,
   config = function()
     require('lint').linters_by_ft = {
       python = { 'flake8' },
       json = { 'jsonlint' },
+      cpp = { 'cpplint' },
     }
 
     vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
