50753eac5a6dcac58bf8db5a7752f9d6f4865ca9
TheRealLorenz <lollobellina@gmail.com>
2024-04-26 11:50:02 +0200

    Add other autocmds

--- lua/user/autocmds.lua
+++ lua/user/autocmds.lua
@@ -1,8 +1,24 @@
--- Highlight when yanking (copying) text
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
   callback = function()
     vim.highlight.on_yank()
   end,
+})
+
+vim.api.nvim_create_autocmd('BufWinEnter', {
+  desc = 'Enter insert mode when moving into terminal',
+  pattern = 'term://*',
+  command = 'startinsert',
+})
+vim.api.nvim_create_autocmd('WinEnter', {
+  desc = 'Enter insert mode when moving into terminal',
+  pattern = 'term://*',
+  command = 'startinsert',
+})
+
+vim.api.nvim_create_autocmd('VimResized', {
+  desc = 'Auto resize panes when resizing nvim window',
+  pattern = '*',
+  command = 'tabdo wincmd =',
 })
