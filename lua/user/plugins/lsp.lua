32c762d01fc3ba9e0bcf33482defe49f1659b5bd
TheRealLorenz <lollobellina@gmail.com>
2024-04-29 22:06:33 +0200

    Load cmake project in clangd without command.json

--- lua/user/plugins/lsp.lua
+++ lua/user/plugins/lsp.lua
@@ -96,7 +96,14 @@ return {
     require('mason').setup()
 
     local servers = {
-      -- clangd = {},
+      clangd = {
+        on_new_config = function(new_config, new_cwd)
+          local status, cmake = pcall(require, 'cmake-tools')
+          if status then
+            cmake.clangd_on_new_config(new_config)
+          end
+        end,
+      },
       -- gopls = {},
       -- pyright = {},
       -- rust_analyzer = {},
--- lua/user/plugins/tools.lua
+++ lua/user/plugins/tools.lua
@@ -4,6 +4,7 @@ return {
     config = function()
       require('cmake-tools').setup {
         cmake_build_directory = 'cmake-build-${variant:buildType}',
+        cmake_soft_link_compile_commands = false,
       }
     end,
     ft = { 'cmake', 'cpp', 'c' },
