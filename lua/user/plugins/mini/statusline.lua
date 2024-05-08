9c787f647dd04d4b3cb5dcb9980677ed230ed4f5
TheRealLorenz <lollobellina@gmail.com>
2024-05-08 17:08:58 +0200

    Refactor code

--- lua/user/plugins/mini/statusline.lua
+++ lua/user/plugins/mini/statusline.lua
@@ -1,4 +1,5 @@
 local statusline = require 'mini.statusline'
+local utils = require 'user.utils'
 
 local section_fileinfo = function(args)
   local filetype = vim.bo.filetype
@@ -32,55 +33,6 @@ local section_fileinfo = function(args)
   return string.format('%s %s %s', filetype, encoding, format)
 end
 
-local buf_unique_names = function()
-  local loaded_bufns = vim.tbl_filter(function(id)
-    return vim.api.nvim_buf_is_loaded(id)
-  end, vim.api.nvim_list_bufs())
-
-  local paths = {}
-  for _, i in ipairs(loaded_bufns) do
-    local name = vim.api.nvim_buf_get_name(i)
-    if name ~= '' then
-      table.insert(paths, name)
-    end
-  end
-
-  local split_paths = vim.tbl_map(function(item)
-    return vim.split(item, '/', { trimempty = true })
-  end, paths)
-
-  while true do
-    local extended = false
-
-    for i = 1, #split_paths do
-      for j = i + 1, #split_paths do
-        local i_len = #split_paths[i]
-        local j_len = #split_paths[j]
-
-        if split_paths[i][i_len] == split_paths[j][j_len] then
-          extended = true
-          split_paths[i][i_len - 1] = split_paths[i][i_len - 1]
-            .. '/'
-            .. table.remove(split_paths[i])
-          split_paths[j][j_len - 1] = split_paths[j][j_len - 1]
-            .. '/'
-            .. table.remove(split_paths[j])
-        end
-      end
-    end
-
-    if not extended then
-      break
-    end
-  end
-
-  local relative_paths = vim.tbl_map(function(item)
-    return item[#item]
-  end, split_paths)
-
-  return relative_paths
-end
-
 local section_filename = function()
   -- In terminal always use plain name
   if vim.bo.buftype == 'terminal' then
@@ -88,14 +40,13 @@ local section_filename = function()
   end
 
   -- Compute unique buf name
-  local path = vim.api.nvim_buf_get_name(0)
-  for _, item in ipairs(buf_unique_names()) do
-    if string.find(path, item) then
-      path = item
-      break
-    end
-  end
+  local full_path = vim.api.nvim_buf_get_name(0)
 
+  local path = utils.tbl_find(function(item)
+    return string.find(full_path, item) ~= nil
+  end, utils.buf_unique_names()) or full_path
+
+  -- Add modifiers
   if vim.bo.modified then
     path = path .. ' [+]'
   end
---
+++ lua/user/utils.lua
@@ -0,0 +1,76 @@
+local M = {}
+
+--- @generic K: any
+--- @generic V: any
+---
+--- @param f fun(v: V): boolean The predicate
+--- @param t table<K, V> The table to search into
+--- @return V? # The first element that matches the predicate, otherwise nil
+M.tbl_find = function(f, t)
+  for _, v in pairs(t) do
+    if f(v) then
+      return v
+    end
+  end
+  return nil
+end
+
+M.buf_unique_names = function()
+  local loaded_bufns = vim.tbl_filter(function(id)
+    return vim.api.nvim_buf_is_loaded(id)
+  end, vim.api.nvim_list_bufs())
+
+  local full_paths = {}
+  for _, i in ipairs(loaded_bufns) do
+    local name = vim.api.nvim_buf_get_name(i)
+    if name ~= '' then
+      table.insert(full_paths, name)
+    end
+  end
+
+  local split_paths = vim.tbl_map(function(item)
+    return vim.split(item, '/', { trimempty = true })
+  end, full_paths)
+
+  local paths = vim.tbl_map(function(segments)
+    local filename = table.remove(segments)
+    return {
+      name = filename,
+      segments = segments,
+      extend = function(self)
+        if #self.segments then
+          self.name = table.remove(self.segments) .. '/' .. self.name
+        end
+      end,
+    }
+  end, split_paths)
+
+  while true do
+    local didExtend = false
+
+    for i = 1, #paths do
+      for j = i + 1, #paths do
+        if paths[i].name == paths[j].name then
+          didExtend = true
+          paths[i]:extend()
+          paths[j]:extend()
+        elseif string.find(paths[i].name, paths[j].name) then
+          didExtend = true
+          paths[j]:extend()
+        end
+      end
+    end
+
+    if not didExtend then
+      break
+    end
+  end
+
+  local relative_paths = vim.tbl_map(function(path)
+    return path.name
+  end, paths)
+
+  return relative_paths
+end
+
+return M
