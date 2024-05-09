cf3eaad1cc1ec2f065d44863a46212153b31b48c
TheRealLorenz <lollobellina@gmail.com>
2024-05-09 12:55:34 +0200

    Add filename to inactive statusline

--- lua/user/statusline/inactive.lua
+++ lua/user/statusline/inactive.lua
@@ -1,7 +1,17 @@
 local conditions = require 'heirline.conditions'
+local utils = require 'heirline.utils'
+
+local Filename = require 'user.statusline.modules.filename'
 
 local Statusline = {
   condition = conditions.is_not_active,
+
+  hl = {
+    fg = utils.get_highlight('StatusLineNC').fg,
+    bg = utils.get_highlight('StatusLineNC').bg,
+  },
+
+  Filename,
 }
 
 return Statusline
