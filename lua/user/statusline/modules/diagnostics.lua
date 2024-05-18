5b812115e06a9b3a2519889fdb5ad9a51a9657fa
TheRealLorenz <lollobellina@gmail.com>
2024-05-18 14:19:25 +0200

    Update diagnostics module

--- lua/user/statusline/modules/diagnostics.lua
+++ lua/user/statusline/modules/diagnostics.lua
@@ -3,7 +3,7 @@ local utils = require 'heirline.utils'
 local diagnostics_for = function(severity)
   return {
     condition = function(self)
-      return (self.diagnostic_count[vim.diagnostic.severity[severity]] or 0) > 0
+      return (self.diagnostic_count[severity] or 0) > 0
     end,
 
     hl = function(self)
@@ -14,7 +14,7 @@ local diagnostics_for = function(severity)
       return string.format(
         '%s %d ',
         self.diagnostic_levels[severity].sign,
-        self.diagnostic_count[vim.diagnostic.severity[severity]]
+        self.diagnostic_count[severity]
       )
     end,
   }
@@ -22,33 +22,38 @@ end
 
 local DiagnosticsBlock = {
   condition = function()
-    return vim.bo.buftype == ''
-      and #vim.lsp.get_active_clients { bufnr = 0 } > 0
+    return vim.bo.buftype == '' and #vim.lsp.get_clients { bufnr = 0 } > 0
   end,
 
   init = function(self)
-    -- From mini.statusline
-    self.diagnostic_count = {}
-    for _, d in ipairs(vim.diagnostic.get(0)) do
-      self.diagnostic_count[d.severity] = (
-        self.diagnostic_count[d.severity] or 0
-      ) + 1
-    end
+    self.diagnostic_count = vim.diagnostic.count(0)
   end,
 
   static = {
     diagnostic_levels = {
-      ERROR = { sign = '', color = utils.get_highlight('diffDeleted').fg },
-      WARN = { sign = '', color = utils.get_highlight('diffChanged').fg },
-      INFO = { sign = '', color = utils.get_highlight('Title').fg },
-      HINT = { sign = '', color = utils.get_highlight('Title').fg },
+      [vim.diagnostic.severity.ERROR] = {
+        sign = '',
+        color = utils.get_highlight('diffDeleted').fg,
+      },
+      [vim.diagnostic.severity.WARN] = {
+        sign = '',
+        color = utils.get_highlight('diffChanged').fg,
+      },
+      [vim.diagnostic.severity.INFO] = {
+        sign = '',
+        color = utils.get_highlight('Title').fg,
+      },
+      [vim.diagnostic.severity.HINT] = {
+        sign = '',
+        color = utils.get_highlight('Title').fg,
+      },
     },
   },
 
-  diagnostics_for 'ERROR',
-  diagnostics_for 'WARN',
-  diagnostics_for 'INFO',
-  diagnostics_for 'HINT',
+  diagnostics_for(vim.diagnostic.severity.ERROR),
+  diagnostics_for(vim.diagnostic.severity.WARN),
+  diagnostics_for(vim.diagnostic.severity.INFO),
+  diagnostics_for(vim.diagnostic.severity.HINT),
 }
 
 return DiagnosticsBlock
