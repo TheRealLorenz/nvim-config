c44dd6b183fdb0b2d2ed245f22f1273e66ab6d4e
TheRealLorenz <lollobellina@gmail.com>
2024-05-30 20:13:10 +0200

    Use builtin snippets

--- lua/user/plugins/cmp.lua
+++ lua/user/plugins/cmp.lua
@@ -3,23 +3,10 @@ return {
   event = 'InsertEnter',
   dependencies = {
     {
-      'L3MON4D3/LuaSnip',
-      build = (function()
-        -- Build Step is needed for regex support in snippets.
-        -- This step is not supported in many windows environments.
-        -- Remove the below condition to re-enable on windows.
-        if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-          return
-        end
-        return 'make install_jsregexp'
-      end)(),
-      dependencies = {
-        {
-          'rafamadriz/friendly-snippets',
-          config = function()
-            require('luasnip.loaders.from_vscode').lazy_load()
-          end,
-        },
+      'garymjr/nvim-snippets',
+      dependencies = { 'rafamadriz/friendly-snippets' },
+      opts = {
+        friendly_snippets = true,
       },
     },
     'saadparwaiz1/cmp_luasnip',
@@ -29,30 +16,16 @@ return {
   },
   config = function()
     local cmp = require 'cmp'
-    local luasnip = require 'luasnip'
-    luasnip.config.setup {}
 
     cmp.setup {
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
-      },
-      -- No, but seriously. Please read `:help ins-completion`, it is really good!
       mapping = cmp.mapping.preset.insert {
         -- Scroll the documentation window [u]p / [d]own
         ['<C-u>'] = cmp.mapping.scroll_docs(-4),
         ['<C-d>'] = cmp.mapping.scroll_docs(4),
 
         ['<CR>'] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            if luasnip.expandable() then
-              luasnip.expand()
-            elseif cmp.get_active_entry() then
-              cmp.confirm { select = false }
-            else
-              fallback()
-            end
+          if cmp.visible() and cmp.get_active_entry() then
+            cmp.confirm { select = false }
           else
             fallback()
           end
@@ -61,8 +34,10 @@ return {
         ['<Tab>'] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
-          elseif luasnip.locally_jumpable(1) then
-            luasnip.jump(1)
+          elseif vim.snippet.active { direction = 1 } then
+            vim.schedule(function()
+              vim.snippet.jump(1)
+            end)
           else
             fallback()
           end
@@ -71,8 +46,10 @@ return {
         ['<S-Tab>'] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
-          elseif luasnip.locally_jumpable(-1) then
-            luasnip.jump(-1)
+          elseif vim.snippet.active { direction = -1 } then
+            vim.schedule(function()
+              vim.snippet.jump(-1)
+            end)
           else
             fallback()
           end
@@ -86,7 +63,7 @@ return {
       sources = {
         { name = 'nvim_lsp_signature_help' },
         { name = 'nvim_lsp' },
-        { name = 'luasnip' },
+        { name = 'snippets' },
         { name = 'path' },
       },
     }
