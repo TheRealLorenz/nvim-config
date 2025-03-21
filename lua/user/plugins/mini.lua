fc6fd3fb289ed6bad0a73e2a2252da7312574d64
TheRealLorenz <lollobellina@gmail.com>
2025-03-21 10:30:48 +0100

    Refactor

--- lua/user/plugins/mini.lua
+++ lua/user/plugins/mini.lua
@@ -1,3 +1,77 @@
+local function bufremove_setup()
+  local bufremove = require 'mini.bufremove'
+  bufremove.setup()
+  vim.keymap.set('n', '<leader>bd', bufremove.delete, { desc = 'Delete' })
+
+  return bufremove
+end
+
+local function files_setup()
+  local files = require 'mini.files'
+  files.setup()
+  vim.keymap.set('n', '<leader>e', files.open, { desc = 'Open Explorer' })
+  return files
+end
+
+local function hipatterns_setup()
+  local hipatterns = require 'mini.hipatterns'
+  hipatterns.setup {
+    highlighters = {
+      -- Highlight standalone 'FIXME', 'XXX', 'HACK', 'TODO', 'NOTE'
+      fixme = {
+        pattern = '%f[%w]()FIXME()%f[%W]',
+        group = 'MiniHipatternsFixme',
+      },
+      xxx = { pattern = '%f[%w]()XXX()%f[%W]', group = 'MiniHipatternsFixme' },
+      hack = {
+        pattern = '%f[%w]()HACK()%f[%W]',
+        group = 'MiniHipatternsHack',
+      },
+      todo = {
+        pattern = '%f[%w]()TODO()%f[%W]',
+        group = 'MiniHipatternsTodo',
+      },
+      note = {
+        pattern = '%f[%w]()NOTE()%f[%W]',
+        group = 'MiniHipatternsNote',
+      },
+
+      -- Highlight hex color strings (`#rrggbb`) using that color
+      hex_color = hipatterns.gen_highlighter.hex_color(),
+    },
+  }
+  return hipatterns
+end
+
+local function notify_setup()
+  local notify = require 'mini.notify'
+  notify.setup()
+  vim.notify = notify.make_notify()
+  return notify
+end
+
+local function pairs_steup()
+  local pairs = require 'mini.pairs'
+  pairs.setup()
+  return pairs
+end
+
+local function icons_setup()
+  local icons = require 'mini.icons'
+  icons.setup()
+  icons.tweak_lsp_kind()
+end
+
+local function snippets_setup()
+  local snippets = require 'mini.snippets'
+  snippets.setup {
+    snippets = {
+      snippets.gen_loader.from_lang(),
+    },
+  }
+  return snippets
+end
+
 return {
   'echasnovski/mini.nvim',
   dependencies = {
@@ -8,63 +82,24 @@ return {
 
     require('mini.surround').setup()
 
-    local bufremove = require 'mini.bufremove'
-    bufremove.setup()
-    vim.keymap.set('n', '<leader>bd', bufremove.delete, { desc = 'Delete' })
+    bufremove_setup()
 
-    local pairs = require 'mini.pairs'
-    pairs.setup()
+    pairs_steup()
 
     require('mini.tabline').setup()
 
-    local files = require 'mini.files'
-    files.setup()
-    vim.keymap.set('n', '<leader>e', files.open, { desc = 'Open Explorer' })
+    files_setup()
 
-    local hipatterns = require 'mini.hipatterns'
-    hipatterns.setup {
-      highlighters = {
-        -- Highlight standalone 'FIXME', 'XXX', 'HACK', 'TODO', 'NOTE'
-        fixme = {
-          pattern = '%f[%w]()FIXME()%f[%W]',
-          group = 'MiniHipatternsFixme',
-        },
-        xxx = { pattern = '%f[%w]()XXX()%f[%W]', group = 'MiniHipatternsFixme' },
-        hack = {
-          pattern = '%f[%w]()HACK()%f[%W]',
-          group = 'MiniHipatternsHack',
-        },
-        todo = {
-          pattern = '%f[%w]()TODO()%f[%W]',
-          group = 'MiniHipatternsTodo',
-        },
-        note = {
-          pattern = '%f[%w]()NOTE()%f[%W]',
-          group = 'MiniHipatternsNote',
-        },
+    hipatterns_setup()
 
-        -- Highlight hex color strings (`#rrggbb`) using that color
-        hex_color = hipatterns.gen_highlighter.hex_color(),
-      },
-    }
-
-    local notify = require 'mini.notify'
-    notify.setup()
-
-    vim.notify = notify.make_notify()
+    notify_setup()
 
     require('mini.statusline').setup()
 
     require('mini.completion').setup()
 
-    require('mini.icons').setup()
-    MiniIcons.tweak_lsp_kind()
+    icons_setup()
 
-    local snippets = require 'mini.snippets'
-    snippets.setup {
-      snippets = {
-        snippets.gen_loader.from_lang(),
-      },
-    }
+    snippets_setup()
   end,
 }
